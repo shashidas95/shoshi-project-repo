85602757a584ad739e25a7ba3fab15368ec60f35
Shashi Kanta Das <60028859+shashidas95@users.noreply.github.com>
2024-11-24 08:14:49 +0000

    updated files check

--- project-xyz/test.py
+++ project-xyz/test.py
@@ -6,7 +6,10 @@ for folder in folders:
     try:
         files = os.listdir(folder)
     except FileNotFoundError:
-        print("please provide a valid folder name")
+        print("please provide a valid folder name it looks like doesnot exists  " + folder )
+        continue
+    except PermissionError:
+        print("currently you dont have permission to aceess the folder/file " + folder)
         continue
     for file in files:
           print(file)
