8b1ea71bd77882d39069fb1731a779308d281d0f
Shashi Kanta Das <60028859+shashidas95@users.noreply.github.com>
2024-11-24 13:11:35 +0000

    modified

--- project-xyz/test1.py
+++ project-xyz/test1.py
@@ -1,5 +1,7 @@
 import requests
 
+# from https://docs.github.com/en/rest/pulls/pulls?apiVersion=2022-11-28 
+# /repos/{owner}/{repo}/pulls 
 response = requests.get("https://api.github.com/repos/kubernetes/kubernetes/pulls")
 details = response.json()
 for i in range(len(details)):
