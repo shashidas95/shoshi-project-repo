6197806d2ebffd463b35d17d184e9bd51f6b2319
Shashi Kanta Das <60028859+shashidas95@users.noreply.github.com>
2024-11-27 15:34:20 +0000

    inheritance

---
+++ test5.py
@@ -0,0 +1,24 @@
+class Student():
+    def __init__(self, name, id):
+        self.name = name
+        self.id = id
+    def details(self):
+        print(self.name, self.id)
+#================================
+class CSEStudent(Student):
+    def __init__(self, name, id, no_labs):
+        super().__init__(name, id)
+        self.no_labs = no_labs
+    def cry(self):
+        print("cse students are crying due to", self.no_labs)
+#==================================
+class BBAStudent(Student):
+    def party(self):
+        print( "all day party")
+#===================================
+cs = CSEStudent("Bob", 11, 10)
+ba = BBAStudent("carol", 33)
+
+cs.cry()
+ba.party()
+
